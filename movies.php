1b91bb673cd69cee0fab9756bc3cee9126feb0f1
bachlava <33270038+bachlava@users.noreply.github.com>
2017-11-25 23:51:18 -0800

    added search functionality
    & populated movies page

--- movies.php
+++ movies.php
@@ -2,8 +2,17 @@
 include('movie_sc_fns.php');
 session_start();
 do_html_header("HBS Movie Rental");
-?>
 
-<?php
+$conn = new mysqli('localhost', 'root', '', 'movierental');
+			
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM movies";
+$result = $conn -> query($sql);
+
+display_movies($result);
+
 do_html_footer();
 ?>
--- output_fns.php
+++ output_fns.php
@@ -430,4 +430,30 @@ function display_form_button($image, $alt) {
            width=\"135\"/></div>";
 }
 
+function display_movies($result) {
+	if ($result -> num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$titarr[] = $row["title"];
+			$dirarr[] = $row["director"];
+			$genarr[] = $row["genre"];
+			$lenarr[] = $row["length"];
+			$relarr[] = $row["releasedate"];
+			$imgarr[] = $row["imageid"];
+		}
+
+		for ($i = 0; $i < count($titarr); $i++) {
+			echo '<div class="movie">';
+			echo '<img src="images/' . $imgarr[$i] . '.jpg" width="182" height="268"/><br/>';
+			echo 'Title: ' . $titarr[$i] . '<br/>';
+			echo 'Director: ' . $dirarr[$i] . '<br/>';
+			echo 'Genre: ' . $genarr[$i] . '<br/>';
+			echo 'Length: ' . $lenarr[$i] . ' minutes<br/>';
+			echo 'Release Date: ' . $relarr[$i] . '<br/>';
+			echo '</div>';
+		}
+	}
+	else {
+		echo "0 matches found";
+	}
+}
 ?>
