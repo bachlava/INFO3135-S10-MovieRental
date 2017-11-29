544bfe58eef91384c163cb43f59f802388246d5f
bachlava <33270038+bachlava@users.noreply.github.com>
2017-11-29 04:01:41 -0800

    Revert "Added login function, not yet fully functional"

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 include('output_fns.php');
-if(!isset($_SESSION)){session_start();}
+session_start();
 do_html_header("HBS Movie Rental");
 do_html_body();
+display_search();
+?>
+
+
+
+<?php
 do_html_footer();
 ?>
